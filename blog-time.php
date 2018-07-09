67265c2fac8567b28589a770e2b163c024345001
Scott Reilly <scott@coffee2code.com>
2018-07-08 23:55:18 -0600

    Update Moment.js to v2.22.2

--- blog-time.php
+++ blog-time.php
@@ -154,7 +154,7 @@ class c2c_BlogTime {
 		wp_enqueue_style( __CLASS__, plugins_url( 'css/blog-time.css', __FILE__ ), array(), self::version() );
 
 		wp_enqueue_script( 'jquery' );
-		wp_enqueue_script( 'momentjs', plugins_url( 'js/moment.js', __FILE__ ), array(), '2.17.1', true );
+		wp_enqueue_script( 'momentjs', plugins_url( 'js/moment.js', __FILE__ ), array(), '2.22.2', true );
 		wp_enqueue_script( __CLASS__, plugins_url( 'js/blog-time.js', __FILE__ ), array( 'jquery', 'momentjs' ), self::version(), true );
 
 		$text = array(
--- readme.txt
+++ readme.txt
@@ -200,6 +200,7 @@ add_filter( 'c2c_blog_time_active_clock', '__return_false' );
 == Changelog ==
 
 = () =
+* Change: Update Moment.js to v2.22.2
 * New: Add README.md
 * New: Add GitHub link to readme
 * Change: Minor whitespace tweaks to unit test bootstrap
